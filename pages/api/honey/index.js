148c657db24c964b51d2cdba700badbff6343316
THECOOLTOAD <alexsaroukhanoff@gmail.com>
2023-06-30 16:30:16 -0700

    commiting small updates

--- pages/api/honey/index.js
+++ pages/api/honey/index.js
@@ -12,7 +12,7 @@ export default withApiAuthRequired(async function handler(req, res) {
   };
   const fetchBody = {
     dataSource: process.env.MONGODB_DATA_SOURCE,
-    database: "honey-bees",
+    database: "honey_bees",
     collection: "honeys",
   };
   const baseUrl = `${process.env.MONGODB_DATA_API_URL}/action`;
